9e4223a194d32f5e1b7061a3aec5a7c7db64eda9
LeoRoma <xiajtn@gmail.com>
2020-12-22 09:46:43 +0000

    validating user input

--- tictactoe.py
+++ tictactoe.py
@@ -5,3 +5,26 @@ row1 =[' ', ' ', ' ']
 row2 =[' ', ' ', ' ']
 row3 =[' ', ' ', ' ']
 display(row1, row2, row3)
+
+def user_choice():
+    
+    choice = 'WRONG'
+    acceptable_range = range(0, 10)
+    within_range = false
+
+
+    while choice.isdigit() == False or within_range == False:
+        choice = input('Please enter a number (0-10): ')
+
+        if choice.isdigit() == False:
+            print('Sorry that is not a digit')
+
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print('Sorry, you are out of acceptable range (0-10)')
+                within_range == False
+
+    return int(choice)
+
